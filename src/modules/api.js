d3567b2b8bc55a38ebb154214d0236993c72461c
Kender ROMAIN <kenderromain@gmail.com>
2023-01-31 15:58:23 -0400

    Apply my chosen styles for the project.

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,4 +1,4 @@
-const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/F2DOFRJJCqkvT8Dgsat4/scores/';
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FgFHyernOmAFGV9BfMNf/scores/';
 
 export default class Api {
   static async post(newPlayer) {
